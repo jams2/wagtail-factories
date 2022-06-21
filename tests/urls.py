f2a2b8da40f2fa4241d5a9edf278d48b4c9855dc
Joshua Munn <public@elysee-munn.family>
2022-06-21 14:26:35 +0100

    Add support for stream block
    Currently failing tests on a struct block that contains a stream
    block, as Factory.__call__ ends up calling _generate on
    StructBlockFactory, which doesn't like the parameters with
    indices (e.g. "inner_stream__0__some_block__some_field=1")

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import re_path
 from wagtail.core import urls as wagtail_urls
 
 urlpatterns = []
 
-urlpatterns += [url(r"^", include(wagtail_urls))]
+urlpatterns += [re_path(r"^", include(wagtail_urls))]
